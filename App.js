7bfc91e1506f62eeacd367fdf55ffa9aa7cf80f2
AmmarMohammed01 <118853010+AmmarMohammed01@users.noreply.github.com>
2024-11-17 11:55:52 -0600

    Main front end component holding files for React

---
+++ App.js
@@ -0,0 +1,18 @@
+import Input from "./components/app-input";
+import Table from "./components/app-table";
+import Title from "./components/app-title";
+import CustomImage from "./components/app-image";
+import "./styles.css"
+
+function App() {
+  return (
+    <>
+      <Title/>
+      <Table/>
+      <CustomImage/>
+      <Input/>
+    </>
+  );
+}
+
+export default App;
